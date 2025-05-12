e8d146e97c422c7a1a7a38993bc62a3989f14a21
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-12 16:59:32 -0300

    Módulo Básico · Capítulo 01 · Vídeo 02

---
+++ capitulo-01/example_01_02.py
@@ -0,0 +1,27 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_01_02.py
+# DATA DE CRIAÇÃO: 08-05-2025
+# DATA DE PUBLICAÇÃO: 12-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+a = 21
+b = 3
+print(a + b)
+
+result = a + b
+print(result)
+
+# Este é um comentário na linguagem de programação Python.
+
+a = 21 # Este é um comentário na linguagem de programação Python.
+print(a)
+
+a = 75 # Este é um comentário na linguagem de programação Python.
+print(a)
+print(b)
+print(a + b)
