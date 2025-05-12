e54dfa716885a5a8e8cc15b7bb09a84669c2dac3
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-12 20:57:58 -0300

    Módulo Básico · Capítulo 02 · Vídeo 02

---
+++ capitulo-02/example_02_02.py
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_02_02.py
+# DATA DE CRIAÇÃO: 09-05-2025
+# DATA DE PUBLICAÇÃO: 12-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+quantity = 2
+print(type(quantity))
+
+unit_price = 36.90
+print(type(unit_price))
+
+total_price = quantity * unit_price
+print(type(total_price))
+
+message = 'O preço total é R$ '
+print(type(message))
