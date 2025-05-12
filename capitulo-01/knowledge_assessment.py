3a1345066287b6e0d88d359ea53f72f4e933ffaa
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-12 17:16:30 -0300

    Módulo Básico · Capítulo 01 · Avaliação de Conhecimentos

---
+++ capitulo-01/knowledge_assessment.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 08-05-2025
+# DATA DE PUBLICAÇÃO: 12-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) Quais são as classes dos objetos simples presentes na linguagem de programação Python?
+# Resposta: int, float, bool, complex.
+
+# Questão 02-) A função nativa type() retorna o tipo do objeto passado como argumento. Quais são os tipos indicados por esta função quando são usados os argumentos 'Python', 2024, 8 == 2**3, 1 / 1?
+# Resposta: str, int, bool, float.
