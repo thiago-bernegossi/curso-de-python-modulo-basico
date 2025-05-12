4e58ff9f7eb3b70706758c8f6fc2aa65148bf16f
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-12 17:07:27 -0300

    Módulo Básico · Capítulo 01 · Vídeo 03

---
+++ capitulo-01/example_01_03.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_01_03.py
+# DATA DE CRIAÇÃO: 08-05-2025
+# DATA DE PUBLICAÇÃO: 12-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+a = 20
+b = 16
+result = a + b
+print(result)
---
+++ capitulo-01/hello_world.py
@@ -0,0 +1,12 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: hello_world.py
+# DATA DE CRIAÇÃO: 08-05-2025
+# DATA DE PUBLICAÇÃO: 12-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+print('Olá, Mundo!')
