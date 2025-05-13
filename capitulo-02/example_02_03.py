d0146d41eca71a536629092a76b62ea58e5b5648
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-12 21:25:15 -0300

    Módulo Básico · Capítulo 02 · Vídeo 03

---
+++ capitulo-02/example_02_03.py
@@ -0,0 +1,28 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_02_03.py
+# DATA DE CRIAÇÃO: 10-05-2025
+# DATA DE PUBLICAÇÃO: 12-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+integer = 12
+print(type(integer))
+
+float = 7.38
+print (type(float))
+
+complex = 1.57 * 3.17j
+print(type(complex))
+
+boolean = True
+print(type(boolean))
+
+boolean = False
+print(type(boolean))
+
+none = None
+print(type(none))
