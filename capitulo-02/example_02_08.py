2f7c2d1a4ebd64421646ff1cc7ac4a4e673c26b6
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-13 18:20:33 -0300

    Módulo Básico · Capítulo 02 · Vídeo 08

---
+++ capitulo-02/example_02_08.py
@@ -0,0 +1,56 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_02_08.py
+# DATA DE CRIAÇÃO: 13-05-2025
+# DATA DE PUBLICAÇÃO: 13-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+object_a = 14
+object_b = 5
+object_c = object_a + object_b
+print((object_c))
+
+object_c = object_a - object_b
+print((object_c))
+
+object_c = object_a * object_b
+print((object_c))
+
+object_c = object_a / object_b
+print((object_c))
+
+object_c = object_a // object_b
+print((object_c))
+
+object_c = object_a % object_b
+print((object_c))
+
+object_c = -object_a
+print((object_c))
+
+object_c = -object_c
+print((object_c))
+
+object_c = object_a ** object_b
+print(object_c)
+
+object_c = 3 ** 2
+print(object_c)
+
+object_a = 2.15
+object_b = 5.90
+object_c = object_a + object_b
+print(object_c)
+
+object_a = 10
+object_b = 25
+object_c = 7
+result = 2 * object_a + object_b
+print(result)
+
+result = 2 * (object_a + object_b)
+print(result)
