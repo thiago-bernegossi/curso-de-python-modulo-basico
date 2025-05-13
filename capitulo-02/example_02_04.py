69f8f7d68aec1b885aa3bcb4b75963c63402ed11
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-12 21:52:16 -0300

    Módulo Básico · Capítulo 02 · Vídeo 04

---
+++ capitulo-02/example_02_04.py
@@ -0,0 +1,26 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_02_04.py
+# DATA DE CRIAÇÃO: 10-05-2025
+# DATA DE PUBLICAÇÃO: 12-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable_a = 10
+print(variable_a)
+print(type(variable_a))
+
+variable_b = variable_a
+print(variable_b)
+print(type(variable_b))
+
+variable_c = variable_a + 10
+print(variable_c)
+print(type(variable_c))
+
+variable_d = len('Exemplo')
+print(variable_d)
+print(type(variable_d))
