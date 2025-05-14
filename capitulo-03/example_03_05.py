e767d704fff8955f6f94d214057e4540d7ebb78a
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-14 17:42:53 -0300

    Módulo Básico · Capítulo 03 · Vídeo 04

---
+++ capitulo-03/example_03_05.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_03_05.py
+# DATA DE CRIAÇÃO: 14-05-2025
+# DATA DE PUBLICAÇÃO: 14-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+message = input('Digite a sua mensagem: ')
+print('A sua mensagem é:', message)
