4beeb02fdacb067d5669b868e21001446c9110e4
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-14 17:06:03 -0300

    Módulo Básico · Capítulo 03 · Vídeo 02

---
+++ capitulo-03/end_of_line.py
@@ -0,0 +1,25 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: end_of_line.py
+# DATA DE CRIAÇÃO: 14-05-2025
+# DATA DE PUBLICAÇÃO: 14-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+object_x = 12
+object_y = 26
+print(object_x)
+print(object_y)
+
+object_x = 12
+object_y = 26
+print(object_x, end='')
+print(object_y)
+
+object_x = 12
+object_y = 26
+print(object_x, end='...')
+print(object_y)
---
+++ capitulo-03/example_03_02.py
@@ -0,0 +1,24 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_03_02.py
+# DATA DE CRIAÇÃO: 14-05-2025
+# DATA DE PUBLICAÇÃO: 14-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+object_a = 12
+object_b = 19
+print(object_a, object_b)
+
+print(object_a, object_b, sep='-')
+print(object_a, object_b, sep=' - ')
+print(object_a, object_b, sep=' <---> ')
+print(object_a, object_b, sep=', ')
+
+print('Um\nDois\nTrês')
+print('Um Dois Três')
+print('Um.Dois.Três')
+print('Um\tDois\tTrês')
