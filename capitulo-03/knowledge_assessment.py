b0d3f305a148b1384cd59f8d7960f868cc699a36
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-14 20:47:24 -0300

    Módulo Básico · Capítulo 03 · Avaliação de Conhecimentos

---
+++ capitulo-03/knowledge_assessment.py
@@ -0,0 +1,20 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 14-05-2025
+# DATA DE PUBLICAÇÃO: 14-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) Considerando uma lista, não vazia, contendo apenas elementos numéricos não ordenados, como pode ser obtido seu valor médio?
+# Resposta: min(lista) / 2 + max(lista) / 2.
+
+# Questão 02-) Considerando que:
+a = 3.5
+b = 2
+# O que é impresso no console quando ser executa:
+print(a / b, a // b, a % b)
+# Resposta: 1.75 1.0 1.5.
