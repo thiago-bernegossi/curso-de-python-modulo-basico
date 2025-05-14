7fe58ed32cd8166ee2169a394571be9d9abb2290
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-14 16:41:48 -0300

    Módulo Básico · Capítulo 03 · Vídeo 01

---
+++ capitulo-03/example_03_01.py
@@ -0,0 +1,26 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_03_01.py
+# DATA DE CRIAÇÃO: 14-05-2025
+# DATA DE PUBLICAÇÃO: 14-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+print('Olá! Esta é uma mensagem de exemplo.')
+
+object_x = 26
+print(object_x)
+
+object_y = 58
+print(object_y)
+
+print(object_x, object_y)
+
+print('O valor de X é', object_x)
+
+print('O valor de X é {0} e o de Y é {1}'.format(object_x, object_y))
+print('O valor de Y é {0} e o de X é {1}'.format(object_y, object_x))
+print('O valor de X é {1} e o de Y é {0}'.format(object_y, object_x))
