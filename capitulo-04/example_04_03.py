1b7afa823b90ae45d6672e66d0baa70cc0adbb50
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-15 20:47:24 -0300

    Módulo Básico · Capítulo 04 · Vídeo 03

---
+++ capitulo-04/example_04_03.py
@@ -0,0 +1,23 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_04_03.py
+# DATA DE CRIAÇÃO: 15-05-2025
+# DATA DE PUBLICAÇÃO: 15-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+object_a = 10
+object_b = 50
+
+print(f'10 > 0: {object_a > 0}.')
+print(f'50 <= 0: {object_b <= 0}.')
+print(f'10 >= 50: {object_a >= object_b}.')
+print(f'10 < 50: {object_a < object_b}.')
+print(f'10 <= 50: {object_a <= object_b}.')
+print(f'5 * 10 == 50: {(5 * object_a) == object_b}.')
+print(f'5 * 10 < 50: {(5 * object_a) < object_b}.')
+print(f'10 >= √50: {object_a >= pow(object_b, 0.5)}.')
+print(f'√50: {pow(object_b, 0.5)}.')
