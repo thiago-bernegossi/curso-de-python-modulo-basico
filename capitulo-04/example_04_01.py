9ebfb5529e371378b440165e587530dc7f993c48
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-15 18:54:20 -0300

    Módulo Básico · Capítulo 04 · Vídeo 01

---
+++ capitulo-04/example_04_01.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_04_01.py
+# DATA DE CRIAÇÃO: 15-05-2025
+# DATA DE PUBLICAÇÃO: 15-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+object_a = 10
+object_b = 0
+result = object_a / object_b
+print(result)
