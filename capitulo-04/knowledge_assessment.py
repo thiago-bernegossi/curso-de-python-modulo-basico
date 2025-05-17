6f3501bd1941c7be03afeb9fab27e8f83a6eb7de
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-17 16:24:52 -0300

    Módulo Básico · Capítulo 04 · Avaliação de Conhecimentos

---
+++ capitulo-04/knowledge_assessment.py
@@ -0,0 +1,26 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 17-05-2025
+# DATA DE PUBLICAÇÃO: 17-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) O Python oferece o if, uma diretiva para o desvio condicional (ou seleção), que possui as seguintes características:
+# I. a decisão simples, com uso apenas do if, avalia uma expressão lógica, permitindo executar ou não um comando ou um conjunto de comandos.
+# II. a decisão completa, que o if' combinado com a cláusula else, avalia uma expressão lógica, permitindo executar um comando (ou conjunto de comandos) dentre dois comandos (ou conjuntos de comandos).
+# III. existe uma cláusula elseif que possibilita o encadeamento de várias diretivas if.
+# Resposta: c. apenas as afirmações I e II são corretas.
+
+# Questão 02-) Analise o fragmento de código que segue.
+a = 5
+while a > 0:
+    print(a)
+    a -= 1
+else:
+    print('!!!')
+# Em relação ao comando subordinado à cláusula else:
+# Resposta: c. o comando é executado uma vez, ao final do loop.
