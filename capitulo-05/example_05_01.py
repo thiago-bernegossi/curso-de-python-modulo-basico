339cff706ab11719aec17ee638122e64a7cf9c9b
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-18 16:36:05 -0300

    Módulo Básico · Capítulo 05 · Vídeo 01

---
+++ capitulo-05/example_05_01.py
@@ -0,0 +1,20 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_05_01.py
+# DATA DE CRIAÇÃO: 18-05-2025
+# DATA DE PUBLICAÇÃO: 18-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+print('A execução do sistema informático foi iniciada.\n')
+
+counter = 1
+
+while counter <= 10:
+    print(counter)
+    counter = counter + 1
+
+print('\nA execução do sistema informático foi concluída.')
