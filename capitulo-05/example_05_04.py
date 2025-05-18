2363c67108c1b120a53436b7784d114398f05de9
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-18 17:47:51 -0300

    Módulo Básico · Capítulo 05 · Vídeo 04

---
+++ capitulo-05/example_05_04.py
@@ -0,0 +1,26 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_05_04.py
+# DATA DE CRIAÇÃO: 18-05-2025
+# DATA DE PUBLICAÇÃO: 18-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+print('A execução do sistema informático foi iniciada.\n')
+
+counter = 1
+
+while counter > 0:
+    counter = int(input('Digite um número inteiro: '))
+    if counter == 0:
+        print(f'O número inteiro digitado foi: {counter}.')    
+        break
+    else:
+        print('Escolha outro número inteiro...\n')
+else:
+    print(f'O número inteiro digitado foi: {counter}.')
+
+print('\nA execução do sistema informático foi concluída.')
