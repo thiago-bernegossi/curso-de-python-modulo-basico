9d5726a3e5eac10ea7660860ea1c5bb1f143db50
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-19 17:35:06 -0300

    Módulo Básico · Capítulo 05 · Avaliação de Conhecimentos

---
+++ capitulo-05/knowledge_assessment.py
@@ -0,0 +1,24 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 19-05-2025
+# DATA DE PUBLICAÇÃO: 19-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) A diretiva while da linguagem de programação Python, semelhante à existente em outras linguagens, possui várias características.
+# Resposta: b. executa condicionalmente um comando ou bloco de comandos; suas iterações podem ser modificadas com uso de break e continue; admitindo ainda uma cláusula else, executada quando a condição é avaliada como False.
+
+# Questão 02-) Analise o fragmento de código que segue.
+a = 0
+b = 10
+c = 0
+while a < b:
+    a += 1
+    b -= 2
+    c += b - a
+# Qual é o valor armazenado na variável c imediatamente após a execução deste trecho?
+# Resposta: d. 10.
