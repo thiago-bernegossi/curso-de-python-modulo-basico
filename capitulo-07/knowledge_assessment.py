0d9ac2e350d4684d3e4bd471f9e4886ecb7e1802
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-26 20:20:37 -0300

    Módulo Básico · Capítulo 07 · Avaliação de Conhecimentos

---
+++ capitulo-07/knowledge_assessment.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 26-05-2025
+# DATA DE PUBLICAÇÃO: 26-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) Qual, dos comandos que seguem, é executado para os valores de n = [0,3,6,9,12,15,18,21,24,27,30]?
+# Resposta: d. for n in range(0, 31, 3): ...
+
+# Questão 02-) Um dos tipos de objetos compostos oferecidos pelo Python é a string (cadeia de caracteres), que corresponde à classe str. São características das strings:
+# Resposta: b. permite o armazenamento homogêneo, imutável, navegável e indexável, ou seja, o armazenamento de uma quantidade fixa de elementos do mesmo tipo, que podem ser percorridos sequencialmente ou acessados individualmente.
